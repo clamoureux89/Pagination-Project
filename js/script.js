12266a42800f4cc0b65cbe8b42ec3bd45bb52baf
clamoureux89 <51682100+clamoureux89@users.noreply.github.com>
2019-06-18 15:01:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,25 +5,25 @@ FSJS project 2 - List Filter and Pagination
 
 function checkForTarget(link){
    if(link != event.target){
-      link.classList.remove('active')
+      link.classList.remove('active');
    }
 }
 
 
-let studentlist = document.querySelector('ul')
-let students = studentlist.getElementsByTagName('li')
+const studentlist = document.querySelector('ul')
+const students = studentlist.getElementsByTagName('li')
 itemsperpage = 10
 
 function ShowPage(list,page) {
-   
+   first = (page *itemsperpage)-(itemsperpage +1);
+   second = (page*itemsperpage) - 1;
    for(i=0; i< list.length;i++){
-      if (i<(page *itemsperpage)-itemsperpage && i >= page*itemsperpage){
-         list[i].style.display === "block"
-         console.log(i+"not changed")
+      if ([i]> first && [i] <= second){
+         list[i].style.display = "block";
+         console.log(i+"not changed");
       }else{
-         list[i].style.visibility === "none"
-         list[i].style.display === "none"
-         console.log(i + "is changed")
+         list[i].style.display = "none";
+         console.log(i + "is changed");
       }
    }
 }
@@ -32,25 +32,29 @@ function ShowPage(list,page) {
 
 
 const appendPageLinks = (list) => {
-   site = document.querySelector('.page')
-   pages = list/itemsperpage
-   pagination = document.createElement('div')
-   pagination.classList.add('pagination')
-   pages_list = document.createElement('ul')
-   pages_list.classList.add('pages-list')
-   pagination.appendChild(pages_list)
-   site.appendChild(pagination)
+   site = document.querySelector('.page');
+   pages = list/itemsperpage;
+   pagination = document.createElement('div');
+   pagination.classList.add('pagination');
+   pages_list = document.createElement('ul');
+   pages_list.classList.add('pages-list');
+   pagination.appendChild(pages_list);
+   site.appendChild(pagination);
    for(i=1;i<pages;i++){
-      icon = document.createElement('li')
-      number = document.createElement('a')
-      number.herf === "#"
-      number.textContent === i
-      pages_list.appendChild(number)
-      document.addEventListener('click',ShowPage(list,event.target))
+      icon = document.createElement('li');
+      number = document.createElement('a');
+      number.herf === "#";
+      number.textContent === i;
+      pages_list.appendChild(number);
+      document.addEventListener('click',() => {
+         ShowPage(list,event.target)
+         event.target.classList.add('active')
+
+      })
    }
 }
 
 
 
-ShowPage(students,1)
-appendPageLinks(students)
+ShowPage(students,1);
+appendPageLinks(students);
